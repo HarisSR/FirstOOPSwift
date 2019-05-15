cd863bcfe4d346e53b8daab659a5d3f7b723574f
Haris <haris.sr21@gmail.com>
2019-05-15 09:23:52 +0700

    Initial Commit

---
+++ FirstOOPSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstOOPSwift
+//
+//  Created by Haris Shobaruddin Roabbni on 15/05/19.
+//  Copyright © 2019 Haris Shobaruddin Robbani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
+
